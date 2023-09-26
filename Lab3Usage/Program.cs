01784199a6fc879a90f37b5b9d5f845b255dd565
Vlad Chernenko <vladchernenko@protonmail.com>
2023-09-27 01:38:28 +0300

    Added Lab3 + Lab3Usage. Going to add illustrative README of how to use

---
+++ Lab3Usage/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab3Usage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string pathToInputFile = "../../../input.txt";
+
+            string pathToOutputFile = "../../../output.txt";
+
+            Lab3.Class1.FindSquareOfMaze(pathToInputFile,pathToOutputFile);
+        }
+    }
+}
